6b3b766037fb6773ed6d67416acfd408c3538582
Emily Y Leung <30677901+emilyyleung@users.noreply.github.com>
2024-11-05 23:28:50 +1100

    Left and right arrows move files to folders

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from os import listdir
+from os import listdir, rename, scandir
 from os.path import isfile, join
 
 from PIL import Image, ImageTk
@@ -9,29 +9,49 @@ class PhotoSifter:
         self.root = root
         self.root.title("Photo Sifter")
 
-        self.path = './photos'
-        self.images = [f for f in listdir(self.path) if isfile(join(self.path, f))]
+        self.path = './photos/'
+        self.total = 0
+
+        for path in scandir(self.path):
+            if (path.is_file()):
+                self.total += 1
         
-        self.count = 0
+        if self.total > 0:
+            self.images = [f for f in listdir(self.path) if isfile(join(self.path, f))]
+            
+            self.count = 0
 
-        self.image_load = Image.open('./photos/' + self.images[self.count])
-        self.image_resized = self.image_load.resize((300, 300))
-        self.image = ImageTk.PhotoImage(self.image_resized)
+            self.image_load = Image.open(self.path + self.images[self.count])
+            self.image_resized = self.image_load.resize((300, 300))
+            self.image = ImageTk.PhotoImage(self.image_resized)
 
-        self.label = tk.Label(image=self.image)
-        self.label.image = self.image
-        self.label.pack()
+            self.label = tk.Label(image=self.image)
+            self.label.image = self.image
+            self.label.pack()
 
-        # Bind arrow key events to actions
-        self.root.bind("<Right>", self.press_right)
-        self.root.bind("<Left>", self.press_left)
-        self.root.bind("<Up>", self.press_up)
-        self.root.bind("<Down>", self.press_down)
+            # Bind arrow key events to actions
+            self.root.bind("<Right>", self.press_right)
+            self.root.bind("<Left>", self.press_left)
+            self.root.bind("<Up>", self.press_up)
+            self.root.bind("<Down>", self.press_down)
 
-        self.root.focus_set()
+            self.root.focus_set()
+        else:
+            print("NO IMAGES AVAILABLE")
+    
+    def reload_images(self):
+        self.images = [f for f in listdir(self.path) if isfile(join(self.path, f))]
+        print(self.images)
+        if len(self.images) == 0:
+            self.label.config(image='')
+            # self.label.image = None
+        elif len(self.images) == 1:
+            self.load_image(self.images[0])
+        else:
+            self.load_image(self.images[self.count])
 
     def load_image(self, image_path):
-        self.image_load = Image.open('./photos/' + image_path)
+        self.image_load = Image.open(self.path + image_path)
         self.image_resized = self.image_load.resize((300, 300))
         self.image = ImageTk.PhotoImage(self.image_resized)
         self.label.config(image=self.image)
@@ -39,16 +59,30 @@ class PhotoSifter:
 
     def press_right(self, action):
         print("RIGHT")
+        current_image = self.images[self.count]
+        current_file = self.path + current_image
+        move_file = self.path + 'upload/' + current_image
+        rename(current_file, move_file)
+        self.reload_images()
 
     def press_left(self, action):
         print("LEFT")
+        current_image = self.images[self.count]
+        current_file = self.path + current_image
+        move_file = self.path + 'skip/' + current_image
+        rename(current_file, move_file)
+        self.reload_images()
 
     def press_up(self, action):
-        print("UP")
+        if (self.count == 0):
+            print("START")
+        else:
+            self.count -= 1
+            print(self.count)
+            new_image = self.images[self.count]
+            self.load_image(new_image)
 
     def press_down(self, action):
-        print("DOWN")
-        print(self.count, len(self.images))
         if self.count == len(self.images) - 1:
             print("FINISHED")
         else:
